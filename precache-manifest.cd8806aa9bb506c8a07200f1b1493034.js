d1424167a625ba96ff7e0caeb8d1ee86b334409e
Stonino82 <antoninlattene@gmail.com>
2020-05-01 20:46:00 +0200

    Updates

---
+++ precache-manifest.cd8806aa9bb506c8a07200f1b1493034.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1be18558bc3a159817bb4da4d062c248",
+    "url": "/Meme-Generator/index.html"
+  },
+  {
+    "revision": "e13a774721d3453b1b77",
+    "url": "/Meme-Generator/static/css/main.46012885.chunk.css"
+  },
+  {
+    "revision": "6d4cae521faab0dbdb16",
+    "url": "/Meme-Generator/static/js/2.0584803a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Meme-Generator/static/js/2.0584803a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e13a774721d3453b1b77",
+    "url": "/Meme-Generator/static/js/main.30cb7b79.chunk.js"
+  },
+  {
+    "revision": "1b87b95a1e8838355eaf",
+    "url": "/Meme-Generator/static/js/runtime-main.2ef1f00a.js"
+  }
+]);
